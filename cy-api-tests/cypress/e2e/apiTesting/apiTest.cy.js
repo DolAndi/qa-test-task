e3f6758058970c46dd69fe7177140751e505f435
Andriano Toazza <anditoazza@gmail.com>
2023-12-27 11:40:52 -0300

    api test v2

--- cy-api-tests/cypress/e2e/apiTesting/apiTest.cy.js
+++ cy-api-tests/cypress/e2e/apiTesting/apiTest.cy.js
@@ -3,8 +3,11 @@ import { fetchAuthApis } from '../../support/requests/apiRequests';
 describe('API Test', () => {
   it('should find APIs with Authentication & Authorization category', () => {
     fetchAuthApis().then((authApis) => {
-      console.log('Found APIs:', authApis);
+      cy.log('Found APIs:', authApis);
       expect(authApis.length).to.be.greaterThan(0);
+      authApis.forEach((api) => {
+        expect(api.Category).to.equal('Authentication & Authorization');
+      });
     });
   });
 });
